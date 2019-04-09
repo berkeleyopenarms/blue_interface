0eac8747a8917fb9c16b73c0b520952e81231cc8
Brent Yi <brentyi@berkeley.edu>
2019-04-08 22:06:57 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
     name="blue_interface",
     version="0.0",
     description="Python API for controlling the Blue robot arm",
-    url="http://github.com/berkeleyopenrobotics/blue_interface",
+    url="http://github.com/berkeleyopenarms/blue_interface",
     author="Rachel Thomasson, Brent Yi, Philipp Wu, Greg Balke",
     author_email="brentyi@berkeley.edu",
     license="BSD",
